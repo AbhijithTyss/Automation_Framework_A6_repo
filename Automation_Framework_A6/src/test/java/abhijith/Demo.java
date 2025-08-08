4c49c9c00e67678777f6ed4511826b1b2a73fa3e
tiwari-2025 <tiwarianiket961@gmail.com>
2025-08-08 11:34:11 +0530

    Added sync fork

---
+++ Automation_Framework_A6/src/test/java/abhijith/Demo.java
@@ -0,0 +1,14 @@
+package abhijith;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.model.Report;
+
+public class Demo {
+	@Test
+	public void launch() {
+		Reporter.log("Learning sync fork",true);
+	}
+}
